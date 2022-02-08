e8faab5e112cc68508d2b42cdc2b0f2d88db3be3
Abraham Peter <theclassical001@gmail.com>
2022-02-08 12:01:14 +0100

    adminController + api route updated with register,login,logou function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,64 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Admin;
 
 class AdminController extends Controller
 {
-    //
+    public function register(Request $request){
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:admins,email',
+            'password' => 'required|string|'
+        ]);
+
+        $admin = Admin::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        $token = $admin->createToken('adminToken')->plainTextToken;
+
+        $response = [
+            'admin' => $admin,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request){
+        $data = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // check admin email
+        $admin = Admin::where('email', $data['email'])->first();
+
+        if(!$admin || !Hash::check($data['password'], $admin->password)){
+            return repsonse(['message' => 'check details']);
+        }
+
+        $token = $admin->createToken('adminToken')->plainTextToken;
+
+        $response = [
+            'admin' => $admin,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+
+    }
+
+    public function logout(Request $request){
+        auth()->guard('admin')->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged out'
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,6 +16,10 @@ use App\Http\Controllers\AuthController;
 */
 Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+//admin register
+Route::post('/boss/register', [App\Http\Controllers\AdminController::class, 'register']);
+Route::post('/boss/login', [App\Http\Controllers\AdminController::class, 'login']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
